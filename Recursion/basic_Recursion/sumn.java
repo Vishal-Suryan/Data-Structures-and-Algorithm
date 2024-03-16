8718abef0d70bacead366b7ebcef8a9f38901aac
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-03-16 21:26:32 +0530

    recursion

--- Recursion/basic_Recursion/sumn.java
+++ Recursion/basic_Recursion/sumn.java
@@ -3,15 +3,16 @@ import java.util.*;
 public class sumn {
 
   public static void main(String[] args) {
+      solve(3,0);
 
-    solve(5);
-    solve(6);
   }
-  public static void solve(int n) {
-    int sum = 0;
-    for (int i = 1; i <= n; i++) {
-      sum += i;
+  public static void solve(int n,int sum) {
+    if(n<1){
+      System.out.println(sum);
+      return ;
     }
-    System.out.println("The sum of the first " + n + " numbers is: " + sum);
+    
+    solve(n-1,sum+n);
+
   }
 }
