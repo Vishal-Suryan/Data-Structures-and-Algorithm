373be4eb23e714a7c832d0c53362945f715933d7
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-04-01 21:39:43 +0530

    recursive insertion sort

---
+++ Recursion/basic_Recursion/insertionSort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class insertionSort {
+
+    static void insertionSort(int arr[],int i,int n){
+        if(i==n){
+            return;
+        }
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            int temp=arr[j-1];
+            arr[j-1]=arr[j];
+            arr[j]=temp;
+            j--;
+        
+        }
+        insertionSort(arr, i+1, n);
+    }
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the size of Array: ");
+        int size= sc.nextInt();
+        int [] arr=new int[size];
+        System.out.print("Enter Elements: ");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Input Array: "+Arrays.toString(arr));
+        insertionSort(arr,0,size);
+        System.out.println("Sorted Array: "+Arrays.toString(arr));
+    }    
+}
