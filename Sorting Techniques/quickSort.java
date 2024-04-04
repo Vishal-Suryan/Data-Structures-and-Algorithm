49de39fb3e4818c2cdc1c27009f9dba576096799
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-04-04 23:15:35 +0530

    sorting

--- Sorting Techniques/quickSort.java
+++ Sorting Techniques/quickSort.java
@@ -1,61 +1,48 @@
 import java.util.*;
-
-class Solution {
-    static int partition(List<Integer> arr, int low, int high) {
-        int pivot = arr.get(low);
-        int i = low;
-        int j = high;
-
-        while (i < j) {
-            while (arr.get(i) <= pivot && i <= high - 1) {
+class quickSort{
+    static int partition(int[] arr,int low,int high){
+        int pivot=arr[low];
+        int i=low;
+        int j=high;
+        while(i<j){
+            while(arr[i]<=pivot && i<=high-1){
                 i++;
             }
-
-            while (arr.get(j) > pivot && j >= low + 1) {
+            while(arr[j]>pivot && j>=low+1){
                 j--;
             }
-            if (i < j) {
-                int temp = arr.get(i);
-                arr.set(i, arr.get(j));
-                arr.set(j, temp);
+            if(i<j){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
             }
         }
-        int temp = arr.get(low);
-        arr.set(low, arr.get(j));
-        arr.set(j, temp);
+        int temp =arr[low];
+        arr[low]=arr[j];
+        arr[j]=temp;
+
         return j;
     }
-
-    static void qs(List<Integer> arr, int low, int high) {
-        if (low < high) {
-            int pIndex = partition(arr, low, high);
-            qs(arr, low, pIndex - 1);
-            qs(arr, pIndex + 1, high);
+    static void quickSort(int[]arr,int low,int high){
+        if(low<high){
+            int pIndex=partition(arr,low,high);
+            quickSort(arr, low, pIndex-1);
+            quickSort(arr, pIndex+1, high);
         }
+        return;
     }
-    public static List<Integer> quickSort(List<Integer> arr) {
-        // Write your code here.
-        qs(arr, 0, arr.size() - 1);
-        return arr;
+    
+    public static void main(String []args){
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the Size of Array: ");
+        int n=sc.nextInt();
+        int [] array=new int[n];
+        System.out.print("Enter Elements: ");
+        for(int i=0;i<n;i++){
+            array[i]=sc.nextInt();
+        }
+        System.out.println("Input Array: "+Arrays.toString(array));
+        quickSort(array, 0, n-1);
+        System.out.println("Output Array: "+Arrays.toString(array));
     }
 }
-
-public class quickSort {
-    public static void main(String args[]) {
-        List<Integer> arr = new ArrayList<>();
-        arr = Arrays.asList(new Integer[] {4, 6, 2, 5, 7, 9, 1, 3});
-        int n = arr.size();
-        System.out.println("Before Using quick Sort: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr.get(i) + " ");
-        }
-        System.out.println();
-        arr = Solution.quickSort(arr);
-        System.out.println("After quick sort: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr.get(i) + " ");
-        }
-        System.out.println();
-    }
-
-} 
