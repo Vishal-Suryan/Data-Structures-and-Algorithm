e771141b3e9a13a759ef77de4403ba4e434095e2
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-04-19 20:07:09 +0530

    maximum consecutive ones

---
+++ Arrays/Easy/consecutiveOne.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class consecutiveOne {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int size=sc.nextInt();
+        int [] arr = new int[size];
+        System.out.print("Enter Elements: ");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        consecutiveOne(arr);
+    }
+    public static void consecutiveOne(int[] arr){
+        int count =0;
+        int maxcount=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==1){
+                count+=1;
+            }else{
+                maxcount=count;
+                count=0;
+            }
+        }
+        if(maxcount>count){
+            System.out.println(maxcount);
+        }else{
+            System.out.println(count);
+
+        }
+        return;
+    }
+}
