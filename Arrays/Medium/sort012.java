ccd32f44c240c566e1c00e5eda9960dde54787b8
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-05-02 16:09:30 +0530

    medium question

---
+++ Arrays/Medium/sort012.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class sort012 {
+    public static void sortColors(int[] nums) {
+        int count_0= 0, count_1= 0, count_2= 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                count_0++;
+            }else if(nums[i]==1){
+                count_1++;
+            }else{
+                count_2++;
+            }
+        }
+        for(int i=0;i<count_0;i++){
+            nums[i]=0;
+        }
+        for(int i=count_0;i<count_0+count_1;i++){
+            nums[i]=1;
+        }
+        for(int i=count_0+count_1;i<nums.length;i++){
+            nums[i]=2;
+        }
+        System.out.println(Arrays.toString(nums));
+        return;
+    }
+
+    public static void main(String[] args){
+        int [] arr = {0,2,2,2,1};
+        sortColors(arr);
+    }
+
+}
