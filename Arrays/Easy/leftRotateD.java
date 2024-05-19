3930bd58a75e34c2ecf751efa975f7e756e2af6e
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-05-19 16:09:24 +0530

    Update leftRotateD.java

--- Arrays/Easy/leftRotateD.java
+++ Arrays/Easy/leftRotateD.java
@@ -37,5 +37,23 @@ public class leftRotateD{
         }
         System.out.print("Left Rotated Array by "+rotateBy+" places: "+Arrays.toString(temp));
     }
-    
+    public static void rightRotate(int[] arr, int k) {
+        int n=arr.length;
+        if (n == 0)
+            return;
+        k = k % n;
+        if (k > n)
+            return;
+        int[] temp = new int[k];
+        for (int i = n - k; i < n; i++) {
+            temp[i - n + k] = arr[i];
+        }
+        for (int i = n - k - 1; i >= 0; i--) {
+            arr[i + k] = arr[i];
+        }
+        for (int i = 0; i < k; i++) {
+            arr[i] = temp[i];
+        }
+    }
+
 }
