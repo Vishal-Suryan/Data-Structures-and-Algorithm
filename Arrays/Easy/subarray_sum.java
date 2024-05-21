362dc6f5e5a749face5ec357f5d99b4125b4605a
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-05-21 15:05:42 +0530

    Update subarray_sum.java

--- Arrays/Easy/subarray_sum.java
+++ Arrays/Easy/subarray_sum.java
@@ -1,8 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class subarray_sum {
     class Solution{
-        // Function for finding maximum and value pair
+        // Method 1 - TC = O(N^2)
         public static int lenOfLongSubarr (int nums[], int N, int K) {
-            //Complete the function
             int len=0;
             for(int i=0;i<N;i++){
                 int sum=0;
@@ -14,6 +16,48 @@ public class subarray_sum {
                 }
             }
             return len;
+        }
+        // Method 2 = TC= O(NlogN)
+        public static int lenOfLongSubar (int nums[], int N, int K) {
+            Map<Integer,Integer> mp=new HashMap<>();
+            int sum=0;
+            int maxLen=0;
+            for(int i=0;i<nums.length;i++){
+                sum+=nums[i];
+                if(sum==K){
+                    maxLen=Math.max(maxLen,i+1);
+                }
+                int rem=sum-K;
+                if(mp.containsKey(rem)){
+                    int len= i - mp.get(rem);
+                    maxLen=Math.max(maxLen,len);
+                }
+                if(!mp.containsKey(sum)){
+                    mp.put(sum,i);
+                }
+            }
+            return maxLen;
+        }
+        public static int lenLongSubarr (int nums[], int n, int K) {
+            //Works Only if the array contains Positive Integers ===> TC = O(2*n)
+            int left=0;
+            int right=0;
+            int sum=nums[0];
+            int maxLen=0;
+            while(right<n){
+                while(right<n && sum>K){
+                    sum-=nums[left];
+                    left++;
+                }
+                if(sum==K){
+                    maxLen=Math.max(maxLen,right-left+1);
+                }
+                right++;
+                if(right<n){
+                    sum+=nums[right];
+                }
+            }
+            return maxLen;
         }
     }
 }
