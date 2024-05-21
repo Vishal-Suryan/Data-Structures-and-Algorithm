fbc67f3bab61c2eae2af047ceceba004ae86920b
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-05-22 03:37:47 +0530

    Update majority.java

--- Arrays/Medium/majority.java
+++ Arrays/Medium/majority.java
@@ -1,5 +1,6 @@
 public class majority {
     public int majorityElement(int[] nums) {
+        // Moore's Voting Algorithm
         int element=0;
         int count=0;
         for(int i=0;i<nums.length;i++){
