e9671dc2f6837034499c524873d2689b28e95bca
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-05-23 00:38:55 +0530

    Create stock.java

---
+++ Arrays/Medium/stock.java
@@ -0,0 +1,13 @@
+public class stock {
+    class Solution {
+        public int maxProfit(int[] prices) {
+            int maxProfit=0;
+            int min= Integer.MAX_VALUE;
+            for(int i=0;i<prices.length;i++){
+                min=Math.min(min,prices[i]);
+                maxProfit=Math.max(maxProfit,prices[i]-min);
+            }
+            return maxProfit;
+        }
+    }
+}
