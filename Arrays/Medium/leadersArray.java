0975d255b335d7072f9b12dd6d46be81b256391f
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-05-23 16:56:53 +0530

    Create leadersArray.java

---
+++ Arrays/Medium/leadersArray.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class leadersArray {
+    class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        int max=arr[arr.length-1];
+        ans.add(max);
+        for(int i=arr.length-2;i>=0;i--){
+            if(arr[i]>=max){
+                max=arr[i];
+                ans.add(arr[i]);
+            }
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
+}
