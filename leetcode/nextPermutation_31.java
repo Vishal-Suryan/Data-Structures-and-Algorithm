41c93e71ea29079a3516edbdaf98efa4ad34dd1b
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-05-23 16:20:22 +0530

    Create nextPermutation_31.java

---
+++ leetcode/nextPermutation_31.java
@@ -0,0 +1,40 @@
+public class nextPermutation_31 {
+    class Solution {
+        public void nextPermutation(int[] nums) {
+            int idx=-1;
+            for(int i= nums.length-2;i>=0;i--){
+                if(nums[i]<nums[i+1]){
+                    idx=i;
+                    break;
+                }
+            }
+            if(idx==-1){
+                for(int i=0;i<nums.length/2;i++){
+                    int temp=nums[i];
+                    nums[i]=nums[nums.length-i-1];
+                    nums[nums.length-i-1]=temp;
+                }
+                return ;
+            }else{
+                for(int i=nums.length-1;i>idx;i--){
+                    if(nums[i]>nums[idx]){
+                        int temp= nums[i];
+                        nums[i]=nums[idx];
+                        nums[idx]=temp;
+                        break;
+                    }
+                }
+                int i=idx+1;
+                int j=nums.length-1;
+                while(i<j){
+                    int temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                    i++;
+                    j--;
+                }
+                return ;
+            }
+        }
+    }
+}
