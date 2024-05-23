86a0c8f477a6b8f38bb74c0a20acd98bcf67518c
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-05-24 02:10:44 +0530

    Create countSumSubarray.java

---
+++ Arrays/Medium/countSumSubarray.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class countSumSubarray {
+    class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        int sum = 0;
+        int ctr = 0;
+        hashMap.put(0, 1);
+        for(int i = 0; i < nums.length; i++){
+            sum+= nums[i];
+            int check = sum - k;
+            if(hashMap.containsKey(check)) {
+                ctr += hashMap.get(check);
+            }
+            if(!hashMap.containsKey(sum))
+                hashMap.put(sum, 1);
+            else{
+                hashMap.replace(sum, hashMap.get(sum)+1);
+            }
+        }
+        return ctr;
+    }
+}
+}
