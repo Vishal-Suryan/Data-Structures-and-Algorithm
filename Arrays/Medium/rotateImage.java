dc07e7fa1db3b4eae78d6c42e84ec06abdccb695
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-05-26 00:16:50 +0530

    Create rotateImage.java

---
+++ Arrays/Medium/rotateImage.java
@@ -0,0 +1,27 @@
+public class rotateImage {
+    class Solution {
+        public static void rotate(int[][] matrix) {
+            int m=matrix.length;
+            for(int i=0;i<m;i++){
+                for(int j=i;j<m;j++){
+                    int temp= matrix[i][j];
+                    matrix[i][j]=matrix[j][i];
+                    matrix[j][i]=temp;
+                }
+            }   
+            for(int x=0;x<matrix.length;x++){
+                int left=0;
+                int right=matrix.length-1;
+                while(left<right){
+                    int temp=matrix[x][right];
+                    matrix[x][right]=matrix[x][left];
+                    matrix[x][left]=temp;
+                    left++;
+                    right--;
+                    
+                }
+            }
+            return;
+        }
+    }
+}
