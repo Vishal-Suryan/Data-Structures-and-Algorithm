317c02ba529b866180bf0a299f42ab22f1d14437
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-06-06 02:26:57 +0530

    Update rotateImage.java

--- Arrays/Medium/rotateImage.java
+++ Arrays/Medium/rotateImage.java
@@ -1,27 +1,33 @@
 public class rotateImage {
-    class Solution {
-        public static void rotate(int[][] matrix) {
-            int m=matrix.length;
-            for(int i=0;i<m;i++){
-                for(int j=i;j<m;j++){
-                    int temp= matrix[i][j];
-                    matrix[i][j]=matrix[j][i];
-                    matrix[j][i]=temp;
-                }
-            }   
-            for(int x=0;x<matrix.length;x++){
-                int left=0;
-                int right=matrix.length-1;
-                while(left<right){
-                    int temp=matrix[x][right];
-                    matrix[x][right]=matrix[x][left];
-                    matrix[x][left]=temp;
-                    left++;
-                    right--;
-                    
-                }
+    public static void rotatedImage(int[][] matrix){
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<m;j++){ 
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
-            return;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n/2;j++){ 
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-1-j];
+                matrix[i][n-1-j]=temp;
+            }
+        }
+        
+        return;
+    }
+    
+    public static void main(String[] args){
+        int [][] matrix={{1,2,3},{4,5,6},{7,8,9}};
+        rotatedImage(matrix); 
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
         }
     }
 }
