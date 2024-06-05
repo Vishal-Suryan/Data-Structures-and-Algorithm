3e2a9554c8aaf9aa4bf1c5ac734254c1cd8bad3f
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-06-06 03:00:32 +0530

    update

--- Arrays/Medium/spiralMatrix.java
+++ Arrays/Medium/spiralMatrix.java
@@ -1,34 +1,43 @@
 import java.util.ArrayList;
 import java.util.List;
-
 public class spiralMatrix {
-    public List<Integer> spiralOrder(int[][] mat) {
+    public static List<Integer> spiralOrder(int[][] mat) {
         List<Integer> ans = new ArrayList<>();
         int n = mat.length;
         int m = mat[0].length;
         int top = 0, left = 0, bottom = n - 1, right = m - 1;
-        while (top <= bottom && left <= right) {
-            for (int i = left; i <= right; i++){
+        while(left<=right && top<=bottom){
+            for(int i=left;i<=right;i++){
                 ans.add(mat[top][i]);
             }
             top++;
-            for (int i = top; i <= bottom; i++){
+            for(int i=top;i<=bottom;i++){
                 ans.add(mat[i][right]);
             }
             right--;
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--){
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
                     ans.add(mat[bottom][i]);
                 }
                 bottom--;
             }
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--){
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
                     ans.add(mat[i][left]);
-                }
+                }  
                 left++;
             }
         }
         return ans;
+    }
+    public static void main(String[] args){
+        int [][] matrix={{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(spiralOrder(matrix)); 
+        /* for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        } */
     }
 }
