577e7d3f0211c05e1ca4a776a9d61020a49b2e2e
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-06-06 01:35:38 +0530

    Update zeroMatrix.java

--- Arrays/Medium/zeroMatrix.java
+++ Arrays/Medium/zeroMatrix.java
@@ -1,41 +1,66 @@
 public class zeroMatrix {
-    class Solution {
-        public void setZeroes(int[][] matrix) {
-            int col0=1;
-            int n = matrix.length;
-            int m = matrix[0].length;
-            for(int i=0;i<n;i++){
-                for(int j=0;j<m;j++){
-                    if(matrix[i][j]==0){
-                        matrix[i][0]=0;
-                        if(j!=0){
-                            matrix[0][j]=0;
-                        }else{
-                            col0=0;
-                        }
-                    }
+    public static void markRow(int n,int[][]matrix,int row){
+        for(int i=0;i<n;i++){
+            matrix[row][i]=-1;
+        }
+        return;
+    }
+    public static void markCol(int m,int[][]matrix,int col){
+        for(int i=0;i<m;i++){
+            matrix[i][col]=-1;
+        }
+        return;
+    }
+    public static void setZeros1(int [][] matrix){ //Brute Force Solution TC = O(N^3)
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    markRow(n,matrix,i);
+                    markCol(m,matrix,j);
                 }
             }
-            for(int i=1;i<n;i++){
-                for(int j=1;j<m;j++){
-                    if(matrix[i][j]!=0){
-                        if(matrix[i][0]==0 || matrix[0][j]==0){
-                            matrix[i][j]=0;
-                        }
-                    }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==-1){
+                    matrix[i][j]=0;
                 }
             }
-            if(matrix[0][0]==0){
-                for(int j=0;j<m;j++){
-                    matrix[0][j]=0;
+        }
+        return;
+    }
+    public static void setZeros2(int [][] matrix){ // Better Solution TC=2*O(N^2) SC=O(n+m)
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int [] hashRow=new int[n];
+        int [] hashCol=new int[m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    hashCol[j]++;
+                    hashRow[i]++;
                 }
             }
-            if(col0==0){
-                for(int i=0;i<n;i++){
-                    matrix[i][0]=0;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(hashCol[j]!=0 || hashRow[i]!=0){
+                    matrix[i][j]=0;
                 }
             }
-            return;
+        }
+        return;
+    }
+    public static void main(String[] args){
+        int [][] matrix={{1,1,1},{1,0,1},{1,1,1}};
+        setZeros2(matrix); 
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
         }
     }
 }
