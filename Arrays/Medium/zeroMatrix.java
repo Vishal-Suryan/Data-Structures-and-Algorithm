96409de53f81ad55f08ff0be6600db8ce729ec75
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-06-06 01:59:40 +0530

    Update zeroMatrix.java

--- Arrays/Medium/zeroMatrix.java
+++ Arrays/Medium/zeroMatrix.java
@@ -53,6 +53,44 @@ public class zeroMatrix {
         }
         return;
     }
+    public static void setZeros3(int [][] matrix){ // Optimal TC=2*O(N^2)
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int col0=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;//markRow
+                    if(j!=0){
+                        matrix[0][j]=0;//markCol
+                    }else{
+                        col0=0;
+                    }
+                }
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+        }
+        if(matrix[0][0]==0){
+            for(int j=0;j<m;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(col0==0){
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
+            }
+        }
+        return;
+    }
+    
     public static void main(String[] args){
         int [][] matrix={{1,1,1},{1,0,1},{1,1,1}};
         setZeros2(matrix); 
