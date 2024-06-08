fbf5c12d913a73631d345fe5f2fc3584ed38982b
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-06-08 17:13:48 +0530

    Create fourSum_18.java

---
+++ leetcode/fourSum_18.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class fourSum_18 {
+    class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res=new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(i!=0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            for(int j=i+1;j<nums.length;j++){
+                if(j>i+1 && nums[j]==nums[j-1]){
+                    continue;
+                }
+                int k=j+1;
+                int m=nums.length-1;
+                while(k<m){
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[m];
+                    if(sum==target){
+                        List<Integer> temp=Arrays.asList(nums[i],nums[j],nums[k],nums[m]);
+                        res.add(temp);
+                        k++;
+                        m--;
+                        while(k<m && nums[k]==nums[k-1]){
+                            k++;
+                        }
+                        while(k<m && nums[m]==nums[m+1]){
+                            m--;
+                        }
+                    }else if(sum<target){
+                        k++;
+                    }else{
+                        m--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
+}
