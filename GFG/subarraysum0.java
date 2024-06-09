2297a49f1612a135d5258d4245ae4babf062de98
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-06-09 16:17:06 +0530

    Create subarraysum0.java

---
+++ GFG/subarraysum0.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class subarraysum0 {
+    class GfG
+{
+    int maxLen(int arr[], int n)
+    {
+        // Your code here
+        Map<Integer,Integer> mp=new HashMap<>();
+        int sum=0;
+        int max=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            if(sum==0){
+                max=i+1;
+            }else{
+                if(mp.get(sum)!=null){
+                    max=Math.max(max,i-mp.get(sum));
+                }else{
+                    mp.put(sum,i);
+                }
+            }
+        }
+        return max;
+    }
+}
+}
