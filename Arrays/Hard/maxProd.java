74ec0c3e683c9b3701cf48fe6f3ec321b5fdb5c7
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-06-13 01:17:06 +0530

    Create maxProd.java

---
+++ Arrays/Hard/maxProd.java
@@ -0,0 +1,25 @@
+
+public class maxProd {
+    public class maxProduct_152 {
+        class Solution {
+            public int maxProduct(int[] nums) {
+                double max=Integer.MIN_VALUE;
+                double prefix=1;
+                double suffix=1;
+                for(int i=0;i<nums.length;i++){
+                    if(prefix==0){
+                        prefix=1;
+                    }
+                    if(suffix==0){
+                        suffix=1;
+                    }
+                    prefix*=nums[i];
+                    suffix*=nums[nums.length-i-1];
+                    max=Math.max(max,Math.max(suffix,prefix));
+                }
+                return (int)max;
+            }
+        }
+    }
+    
+}
