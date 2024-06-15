d777b2c71ec80d3c2e5631610ada69c26827e492
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-06-16 03:03:39 +0530

    Create smallestDiv_1283.java

---
+++ leetcode/smallestDiv_1283.java
@@ -0,0 +1,34 @@
+public class smallestDiv_1283 {
+    class Solution {
+        public static int getThreshold(int[] map, int val){
+            int ans = 0;
+            for(int i : map) {
+                ans += (i + val - 1) / val;
+            }
+            return ans;
+        }
+        static int findLargest(int[] nums){
+            int max=0;
+            for(int num: nums){
+                max=Math.max(num,max);
+            }
+            return max;
+        }
+        public int smallestDivisor(int[] nums, int threshold) {
+            int low=1;
+            int high=findLargest(nums);
+            int ans=-1;
+            while(low<=high){
+                int mid =(low+high)/2;
+                int value=getThreshold(nums,mid);
+                if(value <= threshold){
+                    ans =mid;
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                } 
+            }
+            return ans;
+        }
+    }
+}
