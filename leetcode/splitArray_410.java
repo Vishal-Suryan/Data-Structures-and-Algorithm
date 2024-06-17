91b8fcd1297d9a840a0bc496e319c576cbc8472f
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-06-17 21:30:03 +0530

    Create splitArray_410.java

---
+++ leetcode/splitArray_410.java
@@ -0,0 +1,39 @@
+public class splitArray_410 {
+    class Solution {
+        public static int countSubarray(int[] nums,int val){
+            int count=1;
+            int sum=0;
+            for(int num: nums){
+                if(sum+num<=val){
+                    sum+=num;
+                }else{
+                    count++;
+                    sum=num;
+                }
+            }
+            return count;
+        }
+        public int splitArray(int[] nums, int k) {
+            if(nums.length<k){
+                return -1;
+            }
+            int max=Integer.MIN_VALUE ; 
+            int sum=0;
+            for(int num:nums){
+                max=Math.max(max,num);
+                sum+=num;
+            }
+            int low=max , high=sum;
+            while(low<=high){
+                int mid=(low+high)/2;
+                int count=countSubarray(nums,mid);
+                if(count>k){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+            return low;
+        }
+    }
+}
