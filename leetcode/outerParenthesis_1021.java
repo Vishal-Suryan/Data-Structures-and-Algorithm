0f7a893f137afab00e1fa2b450c63e10d69338bf
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-06-25 15:50:43 +0530

    Create outerParenthesis_1021.java

---
+++ leetcode/outerParenthesis_1021.java
@@ -0,0 +1,22 @@
+public class outerParenthesis_1021 {
+    class Solution {
+        public String removeOuterParentheses(String s) {
+            StringBuilder sb=new StringBuilder("");
+            int x=0;
+            for(int i=0;i<s.length();i++){
+                if(s.charAt(i)=='('){
+                    x++;
+                    if(x>1){
+                        sb.append(s.charAt(i));
+                    }
+                }else if(s.charAt(i)==')'){
+                    x--;
+                    if(x>0){
+                        sb.append(s.charAt(i));
+                    }
+                }
+            }
+            return sb.toString();
+        }
+    }
+}
