c770679fc986e261bae2017fb2f02b7a8ded518b
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-07-02 23:14:33 +0530

    Create sortCharByFreq_451.java

---
+++ leetcode/sortCharByFreq_451.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class sortCharByFreq_451 {
+    class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+        }
+        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a, b) -> frequencyMap.get(b) - frequencyMap.get(a));
+        maxHeap.addAll(frequencyMap.keySet());
+        StringBuilder result = new StringBuilder();
+        while (!maxHeap.isEmpty()) {
+            char current = maxHeap.poll();
+            int count = frequencyMap.get(current);
+            for (int i = 0; i < count; i++) {
+                result.append(current);
+            }
+        }
+        return result.toString();
+    }
+}
+
+}
