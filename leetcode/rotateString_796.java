0876d33480c78c69ac62ec1b78346f0b742e3a83
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-07-02 22:52:55 +0530

    Create rotateString_796.java

---
+++ leetcode/rotateString_796.java
@@ -0,0 +1,12 @@
+public class rotateString_796 {
+    class Solution {
+        public boolean rotateString(String s, String goal) {
+            if (s.length() != goal.length()) {
+                return false;
+            }
+            String concatenated = s + s;
+            return concatenated.contains(goal);
+        }
+    }
+    
+}
