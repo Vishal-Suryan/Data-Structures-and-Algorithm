555547040c98f03c8bb7958792c3bc7c9c93db1b
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-07-02 22:52:32 +0530

    Create roatatestring.java

---
+++ String/Easy/roatatestring.java
@@ -0,0 +1,12 @@
+public class roatatestring {
+    class Solution {
+        public boolean rotateString(String s, String goal) {
+            if (s.length() != goal.length()) {
+                return false;
+            }
+            String concatenated = s + s;
+            return concatenated.contains(goal);
+        }
+    }
+    
+}
