4671804e76e3f60fce5c28c6d63e6fc775ec3aa1
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-07-04 00:56:15 +0530

    Create insert.java

---
+++ LinkedList/OneD_LL/insert.java
@@ -0,0 +1,38 @@
+public class insert {
+    
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+
+class Solution
+{
+    //Function to insert a node at the beginning of the linked list.
+    Node insertAtBeginning(Node head, int x)
+    {
+        // code here
+        Node temp=new Node(x);
+        temp.next=head;
+        return temp;
+    }
+    
+    //Function to insert a node at the end of the linked list.
+    Node insertAtEnd(Node head, int x)
+    {
+        // code here
+        Node newNode=new Node(x);
+        if(head == null)    return newNode;
+        Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=newNode;
+        return head;
+    }
+}
+}
