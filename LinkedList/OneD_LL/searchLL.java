d4240b9b3f9da121f7f23c748380e2289eca4ef5
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-07-03 17:45:03 +0530

    Create searchLL.java

---
+++ LinkedList/OneD_LL/searchLL.java
@@ -0,0 +1,22 @@
+public class searchLL {
+  class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+
+class Solution {
+    static boolean searchKey(int n, Node head, int key) {
+        // Code here
+        Node temp=head;
+        while(n>0){
+            if(temp.data==key){
+                return true;
+            }
+            temp=temp.next;
+            n--;
+        }
+        return false;
+    }
+}    
+}
