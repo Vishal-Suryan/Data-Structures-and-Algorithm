c4726cd83e46d28a549e8580e1764330dc1017bb
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-07-03 17:41:11 +0530

    Create lengthLL.java

---
+++ LinkedList/OneD_LL/lengthLL.java
@@ -0,0 +1,24 @@
+public class lengthLL {
+    class Node{
+        int data;
+        Node next;
+        Node(int a){  data = a; next = null; }
+    }
+    
+    class Solution
+    {
+        //Function to count nodes of a linked list.
+        public static int getCount(Node head)
+        {   
+            //Code here
+            Node temp=head;
+            int count=0;
+            while(temp!= null){
+                count++;
+                temp=temp.next;
+            }
+            return count;
+        }
+    }
+        
+}
