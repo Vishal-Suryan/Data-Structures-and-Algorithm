1698cc0d52c3cc762d0d22daf757462f98eaf18b
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-07-04 00:10:47 +0530

    Create deleteNode_237.java

---
+++ leetcode/deleteNode_237.java
@@ -0,0 +1,16 @@
+public class deleteNode_237 {
+
+     public class ListNode {
+         int val;
+         ListNode next;
+         ListNode(int x) { val = x; }
+    }
+    
+   class Solution {
+       public void deleteNode(ListNode node) {
+           node.val=node.next.val;
+           node.next=node.next.next;
+           return;
+       }
+   }
+}
