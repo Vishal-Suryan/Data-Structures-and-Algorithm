0a3dd065a5ad05a46ba8abb549f95a397c841d33
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-07-04 21:06:18 +0530

    Create insert.java

---
+++ LinkedList/DoublyLL/insert.java
@@ -0,0 +1,40 @@
+
+public class insert {
+    class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int data)
+	{
+	    this.data = data;
+	    next = prev = null;
+	}
+}
+
+class GfG
+{
+    //Function to insert a new node at given position in doubly linked list.
+    void addNode(Node head_ref, int pos, int data)
+	{
+		// Your code here
+		if(head_ref==null)return;
+        
+		Node temp=head_ref;
+		int count=-1;
+		while(temp!=null){
+		    count++;
+		    if(count==pos){
+		        break;
+		    }
+		    temp=temp.next;
+		}
+		Node newNode=new Node(data);
+		newNode.next=temp.next;
+		//temp.next.prev=newNode;
+		temp.next=newNode;
+		newNode.prev=temp;
+	    return ;
+	}
+}
+}
