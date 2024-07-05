323c4515fe8fc61279e774b3be829c715afde249
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-07-05 16:46:03 +0530

    Create revDLL.java

---
+++ LinkedList/DoublyLL/revDLL.java
@@ -0,0 +1,32 @@
+
+public class revDLL {
+    class Node
+{
+    int data;
+    Node next, prev;
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+public static Node reverseDLL(Node  head)
+{
+    //Your code here
+    if(head==null || head.next==null){
+        return head;
+    }
+    Node x=null;
+    Node temp=head;
+    while(temp!=null){
+        x=temp.prev;
+        temp.prev=temp.next;
+        temp.next=x;
+        temp=temp.prev;
+
+    }
+    return x.prev;
+}
+}
