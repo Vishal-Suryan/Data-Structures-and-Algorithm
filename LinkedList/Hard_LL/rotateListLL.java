754032f8fe5998bf7da4ddd631efd1795dd49e8b
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-07-11 03:41:11 +0530

    Create rotateListLL.java

---
+++ LinkedList/Hard_LL/rotateListLL.java
@@ -0,0 +1,38 @@
+
+public class rotateListLL {
+    public class ListNode {
+             int val;
+             ListNode next;
+             ListNode() {}
+             ListNode(int val) { this.val = val; }
+             ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+         }
+        
+       class Solution {
+           public ListNode rotateRight(ListNode head, int k) {
+               if(head==null ||head.next==null){
+                   return head;
+               }
+               ListNode temp=head;
+               int len=1;
+               ListNode tail=head;
+               while(tail.next!=null){
+                   len++;
+                   tail=tail.next;
+               }
+               if(k%len==0){
+                   return head;
+               }
+               k=k%len;
+               int count=len-k-1;
+               tail.next=head;
+               while(count!=0){
+                   count--;
+                   temp=temp.next;
+               }
+               head=temp.next;
+               temp.next=null;
+               return head;
+           }
+       }
+}
