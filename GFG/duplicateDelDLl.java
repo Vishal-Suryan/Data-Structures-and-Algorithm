3c452b212e0eb71981ec5ced69d6047df188622f
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-07-11 01:18:32 +0530

    Create duplicateDelDLl.java

---
+++ GFG/duplicateDelDLl.java
@@ -0,0 +1,32 @@
+
+public class duplicateDelDLl {
+    
+    class Node{
+        int data;
+        Node next,prev;
+        Node(int x){
+            data = x;
+            next = null;
+            prev = null;
+        }
+    }
+    
+    class Solution{
+        Node removeDuplicates(Node head){
+            // Code Here.
+            Node temp=head;
+            while(temp!=null && temp.next!=null){
+                Node nextNode=temp.next;
+                while(nextNode!=null && temp.data==nextNode.data){
+                    nextNode=nextNode.next;
+                }
+                temp.next=nextNode;
+                if(nextNode!=null){
+                    nextNode.prev=temp;
+                }
+                temp=temp.next;
+            }
+            return head;
+        }
+    }
+}
