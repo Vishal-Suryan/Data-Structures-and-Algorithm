c586242e1982f8ed5c4cb9b3e41ad6d199b79fb9
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-11-24 02:04:43 +0530

    added iterative approach

--- Tree/inOrder.java
+++ Tree/inOrder.java
@@ -29,4 +29,22 @@ class inOrder{
             return;
         }
     }
+    public List<Integer> inorderTraversalUsingStack(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if(root==null) return ans;
+        Stack<TreeNode> st =new Stack<>();
+        TreeNode node=root;
+        while(true){
+            if(node!=null){
+                st.push(node);
+                node=node.left;
+            }else{
+                if(st.isEmpty()) break;
+                node = st.pop();
+                ans.add(node.val);
+                node=node.right;
+            }
+        }
+        return ans;
+    }
 }
--- Tree/preOrder.java
+++ Tree/preOrder.java
@@ -28,5 +28,18 @@ class preOrder{
             helper(root.right,ans);
             return;
         }
+        public List<Integer> preorderTraversalUsingStack(TreeNode root) {
+            List<Integer> ans = new ArrayList<>();
+            if(root==null) return ans;
+            Stack<TreeNode> st =new Stack<>();
+            st.push(root);
+            while(!st.isEmpty()){
+                TreeNode node = st.pop();
+                ans.add(node.val);
+                if(node.right!=null) st.push(node.right);
+                if(node.left!=null) st.push(node.left);
+            }
+            return ans;
+        }
     }
 }
