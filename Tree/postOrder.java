145712f61d20a338667c6c23f93d9bab504a38f1
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2024-11-25 01:25:15 +0530

    Update postOrder.java
    added method of traversal using 1 & 2 stack

--- Tree/postOrder.java
+++ Tree/postOrder.java
@@ -28,5 +28,50 @@ class postOrder{
             ans.add(root.val);
             return;
         }
+        public List<Integer> postorderTraversalUsing2Stack(TreeNode root) {
+            List<Integer> ans = new ArrayList<>();
+            if(root==null) return ans;
+            Stack<TreeNode> st1 =new Stack<>();
+            Stack<TreeNode> st2 =new Stack<>();
+            st1.push(root);
+            while(!st1.isEmpty()){
+                root=st1.pop();
+                st2.push(root);
+                if(root.left!=null) st1.push(root.left);
+                if(root.right!=null) st1.push(root.right);
+            }
+            while(!st2.isEmpty()){
+                ans.add(st2.pop().val);
+            }
+            
+            return ans;
+        }
+        public List<Integer> postorderTraversalUsing1stack(TreeNode root) {
+            List<Integer> ans = new ArrayList<>();
+            if(root==null) return ans;
+            Stack<TreeNode> st1 =new Stack<>();
+            TreeNode curr =root;
+            while(curr!=null || !st1.isEmpty()){
+                if(curr!=null){
+                    st1.push(curr);
+                    curr=curr.left;
+                }else{
+                    TreeNode temp=st1.peek().right;
+                    if(temp==null){
+                        temp=st1.peek();
+                        st1.pop();
+                        ans.add(temp.val);
+                        while(!st1.isEmpty() && temp==st1.peek().right){
+                            temp=st1.peek();
+                            st1.pop();
+                            ans.add(temp.val);
+                        }
+                    }else{
+                        curr=temp;
+                    }
+                }
+            }
+            return ans;
+        }
     }
 }
