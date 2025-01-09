faf0caa81444ab21298eabdb81527ac6a1fcac60
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2025-01-09 16:41:01 +0530

    Update twoSumBST.java

--- BinarySearchTree/twoSumBST.java
+++ BinarySearchTree/twoSumBST.java
@@ -1,6 +1,7 @@
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class twoSumBST {
     public class TreeNode {
@@ -30,6 +31,43 @@ public class twoSumBST {
                 if(curr.right!=null) q.offer(curr.right);
             }
             return false;
+        }
+    }
+    class Solution2 {
+        public boolean findTarget(TreeNode root, int k){
+            if(root==null) return false;
+            BSTIterator l=new BSTIterator(root,false);
+            BSTIterator r=new BSTIterator(root,true);
+            int low=l.next();
+            int high=r.next();
+            while(low<high){
+                if(low+high==k) return true;
+                else if(low+high<k) low=l.next();
+                else high=r.next();
+            }
+            return false;
+        }
+    
+    }
+    class BSTIterator {
+        Stack<TreeNode> st=new Stack<>();
+        boolean reverse=true;
+        public BSTIterator(TreeNode root,boolean isRev){
+            reverse=isRev;
+            pushAll(root);
+        }
+        public int next(){
+            TreeNode node=st.pop();
+            if(reverse==true) pushAll(node.left);
+            else pushAll(node.right);
+            return node.val;
+        }
+        public void pushAll(TreeNode root){
+            while(root!=null){
+                st.push(root);
+                if(reverse==true) root=root.right;
+                else root=root.left;
+            }
         }
     }
 }
