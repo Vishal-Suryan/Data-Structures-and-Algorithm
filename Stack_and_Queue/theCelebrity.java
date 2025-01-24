99db4a254fa08f6467dc2d88fa068a2e496ad4cf
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2025-01-24 14:14:54 +0530

    Update theCelebrity.java

--- Stack_and_Queue/theCelebrity.java
+++ Stack_and_Queue/theCelebrity.java
@@ -18,4 +18,32 @@ public class theCelebrity {
             return candidate;
         }
     }
+    class Solution1 {
+        // Function to find if there is a celebrity in the party or not.
+        public int celebrity(int mat[][]) {
+            int top = 0;
+            int down = mat.length - 1;
+    
+            // Narrow down to a potential candidate
+            while (top < down) {
+                if (mat[top][down] == 1) {
+                    top++;
+                } else {
+                    down--;
+                }
+            }
+    
+            // Verify the candidate
+            for (int i = 0; i < mat.length; i++) {
+                if (i == top) continue;
+    
+                // If the candidate knows someone, or not everyone knows the candidate
+                if (mat[top][i] == 1 || mat[i][top] == 0) {
+                    return -1;
+                }
+            }
+    
+            return top;
+        }
+    }
 }
