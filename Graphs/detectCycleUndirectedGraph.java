bcf7191e52a13bebca05d1ca6bee7b6c672b2092
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2025-02-18 16:28:55 +0530

    Update detectCycleUndirectedGraph.java

--- Graphs/detectCycleUndirectedGraph.java
+++ Graphs/detectCycleUndirectedGraph.java
@@ -6,43 +6,73 @@ public class detectCycleUndirectedGraph {
     // ------ BFS Solution ------ //
     class Solution {
     // Function to detect cycle in an undirected graph.
-    class Pair{
-        int first;
-        int second;
-        public Pair(int first, int second){
-            this.first=first;
-            this.second=second;
-        }
-    }
-    public boolean isCycle(ArrayList<ArrayList<Integer>> adj) {
-        // Code here
-        boolean[] vis=new boolean[adj.size()];
-        for(int i = 0; i<adj.size(); i++) vis[i] = false;
-        for(int i = 0; i<adj.size(); i++){
-            if(!vis[i]){
-                if(detectCycle(i, adj, vis)) return true;
+        class Pair{
+            int first;
+            int second;
+            public Pair(int first, int second){
+                this.first=first;
+                this.second=second;
             }
         }
-        return false;
+        public boolean isCycle(ArrayList<ArrayList<Integer>> adj) {
+            // Code here
+            boolean[] vis=new boolean[adj.size()];
+            for(int i = 0; i<adj.size(); i++) vis[i] = false;
+            for(int i = 0; i<adj.size(); i++){
+                if(!vis[i]){
+                    if(detectCycle(i, adj, vis)) return true;
+                }
+            }
+            return false;
+        }
+        private boolean detectCycle(int src, ArrayList<ArrayList<Integer>> adj, boolean[] vis){
+            vis[src]=true;
+            Queue<Pair> q = new LinkedList<>();
+            q.offer(new Pair(src,-1));
+            while(!q.isEmpty()){
+                int node = q.peek().first;
+                int parent = q.peek().second;
+                q.poll();
+                for(int adjacentNode : adj.get(node)){
+                    if(!vis[adjacentNode]){
+                        vis[adjacentNode]=true;
+                        q.offer(new Pair(adjacentNode,node));
+                    }else if(parent!=adjacentNode){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
     }
-    private boolean detectCycle(int src, ArrayList<ArrayList<Integer>> adj, boolean[] vis){
-        vis[src]=true;
-        Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(src,-1));
-        while(!q.isEmpty()){
-            int node = q.peek().first;
-            int parent = q.peek().second;
-            q.poll();
-            for(int adjacentNode : adj.get(node)){
+    // ------ DFS Solution ------ //
+    class DFSSolution {
+        // Function to detect cycle in an undirected graph.
+        public boolean isCycle(ArrayList<ArrayList<Integer>> adj) {
+            // Code here
+            boolean[] vis=new boolean[adj.size()];
+            for(int i = 0; i<adj.size(); i++) vis[i] = false;
+            for(int i = 0; i<adj.size(); i++){
+                if(!vis[i]){
+                    if(detectCycle(i, -1, adj, vis)) return true;
+                }
+            }
+            return false;
+        }
+        private boolean detectCycle(int src, int parent, ArrayList<ArrayList<Integer>> adj,
+        boolean[] vis){
+            vis[src]=true;
+            for(int adjacentNode : adj.get(src)){
                 if(!vis[adjacentNode]){
-                    vis[adjacentNode]=true;
-                    q.offer(new Pair(adjacentNode,node));
+    
+                    if(detectCycle(adjacentNode,src,adj,vis)){
+                        return true;
+                    }
                 }else if(parent!=adjacentNode){
                     return true;
                 }
             }
+            return false;
         }
-        return false;
     }
-}
 }
