6d304050ef60ae8900ad430175d82f9f2cec5eec
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2025-02-23 00:26:44 +0530

    Update cycleInDirectedGraph.java

--- Graphs/cycleInDirectedGraph.java
+++ Graphs/cycleInDirectedGraph.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 public class cycleInDirectedGraph {
     class Solution {
     // Function to detect cycle in a directed graph.
@@ -25,4 +27,43 @@ public class cycleInDirectedGraph {
             return false;
         }
     }
+    // Function to detect cycle in a directed graph. USing BFS Traversal of Graph
+    class SolutionBFS {
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        int size = adj.size();
+        ArrayList<Integer> topoSort = topologicalSort(adj);
+        return !(topoSort.size() == size);
+    }
+    static ArrayList<Integer> topologicalSort(ArrayList<ArrayList<Integer>> adj) {
+        // Your code here
+        int n = adj.size();
+        int[] inDegree = new int[n];
+        for(int i = 0; i<n; i++){
+            for(int itr : adj.get(i)){
+                inDegree[itr]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i<n; i++){
+            if(inDegree[i] == 0){
+                q.offer(i);
+            }
+        }
+        ArrayList<Integer> res = new ArrayList<>();
+        //int i = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            res.add(node);
+            for(int itr : adj.get(node)){
+                inDegree[itr]--;
+                if(inDegree[itr] == 0){
+                    q.offer(itr);
+                }
+            }
+        }
+        return res;
+    }
+}
 }
