d2cec4d209741a6f68cc66b3d49d13e9d2457e57
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2025-10-09 16:47:34 +0530

    Create BreqPotionsMinTime_3494.java

---
+++ leetcode/BreqPotionsMinTime_3494.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long minTime(int[] skill, int[] mana) {
+        int n = skill.length;
+        int m = mana.length;
+        long[] finishTime = new long[n];
+        for(int j = 0; j < m; j++){
+            finishTime[0] += mana[j] * skill[0];
+            for(int i = 1; i < n; i++){
+                finishTime[i] = Math.max(finishTime[i], finishTime[i - 1]) + (long) mana[j] * skill[i];
+            }
+            for(int i = n - 1; i > 0; i--){
+                finishTime[i - 1] = (long) finishTime[i] - mana[j] * skill[i]; 
+            }
+        }
+        return finishTime[n - 1];
+    }
+}
