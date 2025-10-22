c3593856f4f77b9f5be9ce03b8e4b74c42df0016
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2025-10-22 14:45:47 +0530

    Update numOperations_3346.java

--- leetcode/numOperations_3346.java
+++ leetcode/numOperations_3346.java
@@ -1,11 +1,35 @@
+// class Solution {
+//     public int maxFrequency(int[] nums, int k, int numOperations) {
+//         int min = Integer.MAX_VALUE;
+//         int max = Integer.MIN_VALUE;
+//         int ans = 0;
+//         for(int num : nums){
+//             max = Math.max(max, num);
+//             min = Math.min(min, num);
+//         }
+//         int[] freq = new int[max + 1];
+//         for(int num : nums){
+//             freq[num]++;
+//         }
+//         for(int i = 1; i <= max; i++){
+//             freq[i] += freq[i - 1];
+//         }
+//         for(int curr = min; curr <= max; curr++){
+//             int l = Math.max(min, curr - k);
+//             int r = Math.min(max, curr + k);
+//             int f = freq[curr] - freq[curr - 1];
+//             int extra = Math.min(numOperations, freq[r] - freq[l - 1] - f);
+//             ans = Math.max(ans, f + extra);
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public int maxFrequency(int[] nums, int k, int numOperations) {
-        int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
         int ans = 0;
         for(int num : nums){
             max = Math.max(max, num);
-            min = Math.min(min, num);
         }
         int[] freq = new int[max + 1];
         for(int num : nums){
@@ -14,12 +38,15 @@ class Solution {
         for(int i = 1; i <= max; i++){
             freq[i] += freq[i - 1];
         }
-        for(int curr = min; curr <= max; curr++){
-            int l = Math.max(min, curr - k);
+        for(int curr = 0; curr <= max; curr++){
+            if(freq[curr] == 0) continue;
+            int l = Math.max(0, curr - k);
             int r = Math.min(max, curr + k);
-            int f = freq[curr] - freq[curr - 1];
-            int extra = Math.min(numOperations, freq[r] - freq[l - 1] - f);
-            ans = Math.max(ans, f + extra);
+            int totalCount = freq[r] - ((l > 0) ? freq[l - 1] : 0);
+            int currCount = freq[curr] - ((curr > 0) ? freq[curr - 1] : 0);
+            int needConversion = totalCount - currCount; 
+            int maxPossibleFreq = Math.min(numOperations, needConversion) + currCount;
+            ans = Math.max(ans, maxPossibleFreq);
         }
         return ans;
     }
