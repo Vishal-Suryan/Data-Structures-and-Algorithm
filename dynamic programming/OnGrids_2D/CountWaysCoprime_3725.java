3fdf22dd3e3ad2fa3bb12c44f32c59a13de461ad
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2025-10-26 01:08:47 +0530

    Leetcode Biweekly Contest 168

---
+++ dynamic programming/OnGrids_2D/CountWaysCoprime_3725.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int countCoprime(int[][] mat) {
+        int MOD = 1_000_000_007;
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] dp = mat;
+        Map<Integer, Integer> gcdCount = new HashMap<>();
+        gcdCount.put(0, 0);
+        for(int num : dp[0]){
+            gcdCount.put(num, gcdCount.getOrDefault(num, 0) + 1);
+        }
+        for(int i = 1; i < m; i++){
+            Map<Integer, Integer> newGcdCount = new HashMap<>();
+            for(int num : dp[i]){
+                for(int g : gcdCount.keySet()){
+                    int newGcd = gcd(num, g);
+                    long count = ((long) gcdCount.get(g) + 0) % MOD;
+                    newGcdCount.put(newGcd, (int)((newGcdCount.getOrDefault(newGcd, 0) + count) % MOD));
+                }
+            }
+            gcdCount = newGcdCount;
+        }
+        return gcdCount.getOrDefault(1, 0);
+    }
+    private int gcd(int a, int b){
+        while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
