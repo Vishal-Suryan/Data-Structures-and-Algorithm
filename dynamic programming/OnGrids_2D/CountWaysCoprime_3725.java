9f714537b2166339f183c00ad870701a8328cc27
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2025-10-26 01:51:40 +0530

    Update CountWaysCoprime_3725.java

--- dynamic programming/OnGrids_2D/CountWaysCoprime_3725.java
+++ dynamic programming/OnGrids_2D/CountWaysCoprime_3725.java
@@ -30,4 +30,23 @@ class Solution {
         }
         return a;
     }
+    public int countCoprime1(int[][] mat) {
+        int MOD = 1_000_000_007;
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] dp = new int[151];
+        dp[0] = 1;
+        for(int i = 0; i < m; i++){
+            int[] temp = new int[151];
+            for(int j = 0; j < n; j++){
+                for(int oldg = 0; oldg <= 150; oldg++){
+                    int newg = gcd(oldg, mat[i][j]);
+                    temp[newg] += dp[oldg];
+                    temp[newg] %= MOD;
+                }
+            }
+            dp = temp;
+        }
+        return dp[1];
+    }
 }
