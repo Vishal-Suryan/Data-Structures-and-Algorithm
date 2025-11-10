a2ddafda2c85baef047e91633a89deb4c9e57c58
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2025-11-10 23:36:09 +0530

    Create MInOperationToConvertToZero_3542.java

---
+++ Stack_and_Queue/MInOperationToConvertToZero_3542.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+        int ops = 0;
+        for(int num : nums){
+            while(!st.isEmpty() && st.peek() > num) st.pop();
+            if(num == 0) continue;
+            if(st.isEmpty() || st.peek() < num){
+                ops++;
+                st.push(num);
+            }
+        }
+        return ops;
+    }
+}
